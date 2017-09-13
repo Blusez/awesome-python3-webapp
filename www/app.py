1b52370926ba71e86aabff451c16ad2d673ba440
kellerz <714770824@qq.com>
2017-09-13 17:05:59 +0800

    1

--- www/app.py
+++ www/app.py
@@ -13,22 +13,6 @@ import asyncio, os, json, time
 from datetime import datetime
 
 from aiohttp import web
-
-# def index(request):
-#     return web.Response(body=b'<h1>Awesome</h1>')
-
-# @asyncio.coroutine
-# def init(loop):
-#     app = web.Application(loop=loop)
-#     app.router.add_router('GET','/',index)
-#     srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
-#     logging.info('server started at http://127.0.0.1:9000...')
-#     return srv
-
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(init(loop))
-# loop.run_forever()
-
 def index(request):
     return web.Response(body=b'<h1>Awesomeff</h1>',content_type='text/html')
 
@@ -42,6 +26,7 @@ def index(request):
 # loop = asyncio.get_event_loop()
 # loop.run_until_complete(init(loop))
 # loop.run_forever()
+
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
